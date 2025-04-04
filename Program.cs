f1948b3061a3ba8ac53481d7c4e32b113ac0476c
ccheung8 <calvincheung881@gmail.com>
2025-04-03 18:52:04 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿for (int row = 0; row < 6; row++) { 
+  for (int col = 0; col < (5 - row); col++) {
+    Console.Write(" ");
+  }
+  for (int col = 0; col < row + row + 1; col++) {
+  Console.Write("*");
+  }
+  Console.WriteLine();
+}
